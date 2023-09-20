3e1d2dcd8b8322a0a51cb8d65e6923146c2c5db7
Richard <richardsanvie@gmail.com>
2023-09-20 10:01:06 -0300

    add casting

---
+++ src/functions.js
@@ -0,0 +1,17 @@
+"use strict";
+function calculaArea(base, altura) {
+    return base * altura;
+}
+const calculaArea2 = (base, altura) => base * altura;
+function somar(...numeros) {
+    console.log(numeros);
+}
+function teste() {
+    if (10 > 5) {
+        return 'dez maior que cicno';
+    }
+    else {
+        return ('nono');
+    }
+}
+const resultadoDeTeste = teste(); //retorna string ou number
