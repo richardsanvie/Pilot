27abead8e3a3b96ed672413f01d73bbd6566c658
Richard <richardsanvie@gmail.com>
2023-09-20 12:43:43 -0300

    add dom

---
+++ src/dom.js
@@ -0,0 +1,5 @@
+"use strict";
+const campoNome = document.getElementById('nome');
+const form = document.getElementById('form');
+const botaoEnviar = document.getElementById('btn-enviar');
+botaoEnviar === null || botaoEnviar === void 0 ? void 0 : botaoEnviar.addEventListener('click', (e) => { });
---
+++ src/dom.ts
@@ -0,0 +1,5 @@
+const campoNome = document.getElementById('nome')
+const form = document.getElementById('form')
+const botaoEnviar = document.getElementById('btn-enviar')
+
+botaoEnviar?.addEventListener('click', (e: MouseEvent) => { })
